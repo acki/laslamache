9800a87ec701738d90a52d543a3e57c0f4be8b1f
Jonas Baumann <jone@jone.ch>
2009-11-14 15:35:22 +0100

    added formtastic

--- config/environment.rb
+++ config/environment.rb
@@ -30,6 +30,7 @@ Rails::Initializer.run do |config|
   config.gem 'haml', :version => '>2.2.0'
   config.gem 'compass'
   config.gem 'ancestry'
+  config.gem 'formtastic'
   #config.gem 'chriseppstein-compass', :version => '0.8.9', :source => 'http://gems.github.com/'
 
   # Only load the plugins named here, in the order given. By default, all plugins
