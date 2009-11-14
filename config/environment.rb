32e4aef09c6bacef5919b0cfbbbafe483530ba9c
Jonas Baumann <jone@jone.ch>
2009-11-14 14:37:50 +0100

    added propper dependency to compass

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'haml', :version => '>2.2.0'
+  config.gem 'compass'
   #config.gem 'chriseppstein-compass', :version => '0.8.9', :source => 'http://gems.github.com/'
 
   # Only load the plugins named here, in the order given. By default, all plugins
