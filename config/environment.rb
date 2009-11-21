a98555fd71afbf6223c6060f2e33441a6ea2cfb3
Jonas Baumann <jone@jone.ch>
2009-11-21 11:44:17 +0100

    added sources to gem requires

--- config/environment.rb
+++ config/environment.rb
@@ -28,9 +28,9 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'haml', :version => '>2.2.0'
-  config.gem 'compass'
-  config.gem 'ancestry'
-  config.gem 'formtastic'
+  config.gem 'compass', :source => 'http://www.gemcutter.org/'
+  config.gem 'ancestry', :source => 'http://www.gemcutter.org/'
+  config.gem 'formtastic', :source => 'http://www.gemcutter.org/'
   #config.gem 'chriseppstein-compass', :version => '0.8.9', :source => 'http://gems.github.com/'
 
   # Only load the plugins named here, in the order given. By default, all plugins
